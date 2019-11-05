082171aeb5bf38062f8caf617e773c77da97bbc8
Ba-Zen <bazenatlas@gmail.com>
2019-11-04 19:12:46 -0500

    post example

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("It's alive!");
+// import express from 'express'; // ES Modules
+const express = require('express'); // CommonJS modules, and it is equivalent to line above
+
+const server = express(); // creates server
+
+// request/route handlers to / on localhost:8000
+server.get('/', (req, res) => {
+  res.send('Hello Node Man!');
+});
+
+server.post('/echo', (req, res) => {
+  const data = req.body;
+
+  res.json(data);
+});
+
+// listens for requests in a particular port on localhost
+const port = 8000; // localhost:8000
+server.listen(port, () => console.log('\n=== API on port 8000 ===\n'));
